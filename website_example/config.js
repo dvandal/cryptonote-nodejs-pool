455634d2dd390969b285fd1b23ff3d01bbcf340a
root <root@GRAFTpool.ca>
2018-02-27 14:04:57 -0500

    Add a new Market page with Cryptonator for current market prices. Move Estimate Mining Profit to the new Market page.

--- website_example/config.js
+++ website_example/config.js
@@ -5,6 +5,9 @@ var email = "support@poolhost.com";
 var telegram = "https://t.me/YourPool";
 var discord = "https://discordapp.com/invite/YourPool";
 
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+var defaultCurrency = 'USD';
+
 var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
 var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
 
