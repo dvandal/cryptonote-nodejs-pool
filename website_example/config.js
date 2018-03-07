d681cb9854f32b036f79ebe4a6dc9060ac39c125
root <daniel@vandal.ca>
2018-03-06 22:47:07 -0500

    Rename cryptonatorWidget configuration to marketCurrencies

--- website_example/config.js
+++ website_example/config.js
@@ -5,8 +5,7 @@ var email = "support@poolhost.com";
 var telegram = "https://t.me/YourPool";
 var discord = "https://discordapp.com/invite/YourPool";
 
-var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
-var defaultCurrency = 'USD';
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
 var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
 var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
