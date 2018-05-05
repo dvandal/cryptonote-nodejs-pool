c3a713c394f9907eaef781ae1028d513d9fdf99e
Daniel Vandal <daniel@vandal.ca>
2018-05-05 18:21:36 -0400

    Update config.js

--- website_example/config.js
+++ website_example/config.js
@@ -12,5 +12,5 @@ var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
 
 var themeCss = "themes/default.css";
 
-var langs = { 'en': 'English', 'fr': 'Français', 'ca': 'Català', 'es': 'Español' };
+var langs = { 'en': 'English', 'es': 'Español', 'fr': 'Français', 'ca': 'Català' };
 var defaultLang = 'en';
