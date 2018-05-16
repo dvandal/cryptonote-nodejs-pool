ee01463763e295b5183c4196c33b0b2ce0886e30
root <daniel@vandal.ca>
2018-05-16 10:56:32 -0400

    Fix issue when hashrate is 0.

--- website_example/js/common.js
+++ website_example/js/common.js
@@ -199,11 +199,15 @@ function getReadableTime(seconds){
 
 // Get readable hashrate
 function getReadableHashRateString(hashrate){
+    if (!hashrate) hashrate = 0;
+
     var i = 0;
     var byteUnits = [' H', ' KH', ' MH', ' GH', ' TH', ' PH' ];
-    while (hashrate > 1000){
-        hashrate = hashrate / 1000;
-        i++;
+    if (hashrate > 0) {
+        while (hashrate > 1000){
+            hashrate = hashrate / 1000;
+            i++;
+        }
     }
     return hashrate.toFixed(2) + byteUnits[i];
 }
