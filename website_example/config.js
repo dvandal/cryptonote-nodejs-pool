5517aaa9b3b04832bfc0924be0ccf39cca49059c
muscleman <musclesonvacation@hotmail.com>
2019-06-27 22:07:09 -0500

    Update config.js
    dodge and tether

--- website_example/config.js
+++ website_example/config.js
@@ -5,7 +5,7 @@ var telegram = "https://t.me/YourPool";
 var discord = "https://discordapp.com/invite/YourPool";
 var facebook = "https://www.facebook.com/<YourPoolFacebook>";
 
-var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
 var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
 var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
