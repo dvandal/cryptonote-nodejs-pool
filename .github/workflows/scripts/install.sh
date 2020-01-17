1106da90f3fee7071d143682c70d899f4fcefc31
muscleman <musclesonvacation@hotmail.com>
2020-01-16 20:23:38 -0600

    Update install.sh

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -3,7 +3,7 @@
 #apt-get install libboost-all-dev
 
 # json files
-js-beautify $(find . -type f -name '*.json') -t -n --space-after-named-function --space-after-anon-function -B --good-stuff```
+js-beautify $(find . -type f -name '*.json') -t -n --space-after-named-function --space-after-anon-function -B --good-stuff
 
 # javascript, html and css files
 js-beautify $(find . -type f -name '*.js') $(find . -type f -name '*.html') $(find . -type f -name '*.css') -t -n --space-after-named-function --space-after-anon-function -B --good-stuff
