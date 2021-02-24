d0d83af73ed71fa661daf0d63e512c0530630d91
root <root@miner.vandal.ca>
2021-02-24 14:37:33 -0500

    Update initial website configuration example.

--- website_example/config.js
+++ website_example/config.js
@@ -1,18 +1,19 @@
-var api = "https://your-api.pool.com/apiMerged";
-var apiMerged = "https://your-api.pool.com/apiMerged1";
-let parentCoin = "Chimera"
+var api = "http://poolhost:8117";
+var poolHost = "poolhost.com";
 
 var email = "support@poolhost.com";
 var telegram = "https://t.me/YourPool";
 var discord = "https://discordapp.com/invite/YourPool";
-var facebook = "https://www.facebook.com/<YourPoolFacebook>";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
 var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
 var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
-var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
 
 var themeCss = "themes/default.css";
-var defaultLang = 'en';
+var defaultLang = "en";
+
+# Merged Mining:
+# var api = "http://poolhost/apiMerged";
+# var blockchainExplorer = "http://explorer.ird.cash/?hash={id}#block";
+# var transactionExplorer = "http://explorer.ird.cash/?hash={id}#transaction";
